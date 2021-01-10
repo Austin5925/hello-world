93fe59d36642aef7a572ac2a02bb8e31f334385f
Austin Zhou <72800925+Austin5925@users.noreply.github.com>
2021-01-10 23:53:09 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int isprime(int i) {
+	int ret = 1;
+	int j;
+	for (j = 2; j < i; j++) {
+		if (i % j == 0) {
+			ret = 0;
+			break;
+		}
+	}
+	return ret;
+}
+
+void bubble_sort(int arr[], int len) {
+	int i, j, temp;
+	for (i = 0; i < len - 1; i++)
+		for (j = 0; j < len - 1 - i; j++)
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+}
+
+int main(void) {
+	int n, cnt = 1, a;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a);
+		if (isprime(a)) {
+			printf("%d\n", a);
+		}
+		else {
+			int* b = (int*)malloc(cnt * sizeof(int));
+			while (a != 1) {
+				for (int j = 2; j <= a; j++) {
+					if (isprime(j) && a % j == 0) {
+						if (j != a) {
+							b[cnt - 1] = j;
+							cnt++;
+						}
+						else
+							b[cnt - 1] = j;
+						a /= j;
+					}
+				}
+			}
+			bubble_sort(b, cnt);
+			for (int k = 0; k < cnt; k++) {
+				printf("%d", b[k]);
+				if (k < cnt - 1)
+					printf("*");
+				else
+					printf("\n");
+			}
+		}
+	}
+	return 0;
+}
